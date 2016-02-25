bf4ed0621d2862c83da5ea929113c3595ddad10e
josmrhyde <iherjos@comunidad.unam.mx>
2016-02-24 18:07:04 -0600

    Cambié Hola Mundo por mis datos. Muchas gracias asesor.

--- HolaMundo/HolaMundo/App.cs
+++ HolaMundo/HolaMundo/App.cs
@@ -9,7 +9,7 @@ namespace HolaMundo
 		{	
 			return new ContentPage { 
 				Content = new Label {
-					Text = "Hello world!",
+					Text = "Hola Asesor... mis datos son: Josué Eli Hernández Molina. 402008176. saludos",
 					VerticalOptions = LayoutOptions.CenterAndExpand,
 					HorizontalOptions = LayoutOptions.CenterAndExpand,
 				},
