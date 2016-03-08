30d7118e8690906d87c24b81418b4c01f0c42812
josmrhyde <iherjos@comunidad.unam.mx>
2016-03-08 17:31:24 -0600

    Tuve que revisar sus siguientes actividades porque no me corría. Gracias

--- Actividad3/Actividad3/App.cs
+++ Actividad3/Actividad3/App.cs
@@ -7,13 +7,51 @@ namespace Actividad3
 	{
 		public static Page GetMainPage ()
 		{	
-			return new ContentPage { 
-				Content = new Label {
-					Text = "Hello, Forms!",
-					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
-				},
+			Label etiqueta = new Label {
+				Text = "Imprime tu nombre\nen color:",
+				TextColor = Color.White
 			};
+
+			Button btnNA = new Button
+			{
+				Text = "Agua"
+			};
+
+			btnNA.Clicked += (sender, e) => {
+				etiqueta.Text = "Hernández Molina,\nJosué Eli:\n402008176";
+				etiqueta.TextColor=Color.Aqua;
+			};
+
+			Button btnNO = new Button
+			{
+				Text = "Olivo"
+			};
+
+			btnNO.Clicked += (sender, e) => {
+				etiqueta.Text = "Hernández Molina,\nJosué Eli:\n402008176";
+				etiqueta.TextColor=Color.Olive;
+			};
+
+			//Stacklayout permite apilar los controles verticalmente
+			StackLayout stackLayout = new StackLayout
+			{
+				Children =
+				{
+					etiqueta,
+					btnNA,
+					btnNO
+				}
+				};
+
+			ContentPage contentPage = new ContentPage ();
+			contentPage.Content = stackLayout;
+
+			//Padding agrega un margen al contenido
+			//Device.OnPlatform permite modificar este margen dependiendo de la plataforma IOS, Android y Windows Phone
+			//Para saber más sobe Device.OnPlatform revisa 
+			contentPage.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
+
+			return contentPage;
 		}
 	}
 }
